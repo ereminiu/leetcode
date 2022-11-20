cc9b58466477858ea30aece56e1a5fbc7037de74
ereminiu <ereminiu@gmail.com>
2022-11-20 14:00:52 +0300

    Add task 560

---
+++ task_560.py
@@ -0,0 +1,19 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        pref = n * [0]
+        pref[0] = nums[0]
+        counter = Counter()
+        counter[nums[0]] = 1
+        counter[0] += 1
+        ans = 0
+        
+        if pref[0] == k:
+            ans += 1
+        
+        for i in range(1, n):
+            pref[i] = pref[i-1] + nums[i]
+            ans += counter[pref[i]-k]
+            counter[pref[i]] += 1
+        
+        return ans
