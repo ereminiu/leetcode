ef951cbf0993e24849e45c9d52ca916575cabc04
Ivan Eremin <ereminiu@gmail.com>
2022-12-31 12:58:28 +0300

    Edit cache solution for task 801

--- task_801_cache.py
+++ task_801_cache.py
@@ -1,23 +1,27 @@
 from typing import List
+from functools import cache
 
 class Solution:
     def minSwap(self, a: List[int], b: List[int]) -> int:
         n = len(a)
         inf = int(1e9+228)
 
-        dp = [[inf for x in range(2)] for y in range(n)]
-        dp[0][0], dp[0][1] = 0, 1
-
-        for i in range(1, n):
-            dp[i][0], dp[i][1] = inf, inf
+        @cache
+        def solve(i, k):
+            if i == 0 and k == 0:
+                return 0
+            if i == 0 and k == 1:
+                return 1
+            
+            ret = inf
             if a[i] > a[i-1] and b[i] > b[i-1]:
-                dp[i][0] = min(dp[i][0], dp[i-1][0])
-                dp[i][1] = min(dp[i][1], dp[i-1][1] + 1)
+                ret = min(ret, solve(i-1, k) + k)
             if a[i] > b[i-1] and b[i] > a[i-1]:
-                dp[i][0] = min(dp[i][0], dp[i-1][1])
-                dp[i][1] = min(dp[i][1], dp[i-1][0]+1)
+                ret = min(ret, solve(i-1, k^1) + k)
+            
+            return ret
         
-        return min(dp[n-1][0], dp[n-1][1])
+        return min(solve(n-1, 0), solve(n-1, 1))
 
 print(Solution().minSwap(a=[0,7,8,10,10,11,12,13,19,18], b=[4,4,5,7,11,14,15,16,17,20]))
 print(Solution().minSwap(a = [1,3,5,4], b = [1,2,3,7]))
